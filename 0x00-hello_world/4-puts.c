81d5de2ca561d73c25516947da57f8a020794476
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-07 15:11:15 -0700

    a program that prints out programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * int main - the main function
+ * main - the main function
  * return: 0 if successful
- * Description - a program that prints programming
- * is like building a multilingual puzzle
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
