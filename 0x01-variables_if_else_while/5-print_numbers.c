f623069de3a21869c20e37a141e9c8d555e0afaa
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-08 16:57:19 -0700

    a program that prints out the single digits of base 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0 when success
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+
+	return (0);
+}
