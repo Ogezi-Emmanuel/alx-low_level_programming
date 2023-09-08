6267c72d6fc59643908a2d96a8c3db4b8f137bac
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-08 16:18:12 -0700

    a program that write out all the alphabet letterss in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: o when successful
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
