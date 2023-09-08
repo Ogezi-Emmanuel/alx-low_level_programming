e15a048bb735b24937a007e04b10897c606cdca3
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-07 17:02:21 -0700

    a program that prints the end of Dora Korper quote in 2015

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 1 when successful
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korper, 2015-10-19\n", stdout);
+	
+	return (0);
+}
