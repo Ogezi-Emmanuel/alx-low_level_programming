567b9999091b86d0847e99549367e7049e71d8d1
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-09 15:36:54 -0700

    a program that prints out all the numbers in base 16 and the letters in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	char x = 0;
+
+	while (x < 10)
+	{
+		putchar('0' + x);
+		x++;
+	}
+	x = 0;
+	while (x < 6)
+	{
+		putchar('a' + x);
+		x++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
