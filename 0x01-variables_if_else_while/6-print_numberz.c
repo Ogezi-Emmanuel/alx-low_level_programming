abaa713562a4aacff50dda29ece1f20139269d56
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-08 17:08:11 -0700

    a program that prints out all the single digits of base 10 starting from 0 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		x++;
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
