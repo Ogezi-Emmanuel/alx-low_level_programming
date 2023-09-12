43c7856a4feb9eebf2aba17346cf31ad3a4f50ce
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-12 15:43:56 -0700

    readme file and task o solution

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0 when success
+ */
+
+int mainn(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+first nested loops project readme file
