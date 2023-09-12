25b11c9361b33ca5e66ecd2d20aae5dd435ec73a
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-12 16:18:40 -0700

    a function that prints the alphabets in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,7 @@
 #include "main.h"
 
 /**
- * void print_alphabet(void) - the main file
  * print_alphabet - Prints the lower alphabet followed by a new line
- * Return: 0 when successful
  */
 
 void print_alphabet(void)
@@ -18,6 +16,4 @@ void print_alphabet(void)
 	}
 
 	_putchar('\n');
-
-	return (0);
 }
