854196540b7b33ea0adea1c693773e82aa3e6fc4
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-09-26 10:29:46 -0700

    the memset function

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,14 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	char *i = s;
 
-	for (i = 0, i < n; i++)
+	while (n > 0)
 	{
-		s[i] = b;
+		*s = b;
+		s++;
+		n--;
 	}
 
-	return (s);
+	return (i);
 }
