6992345042cf64a6fbc72120efc413fb72798b0d
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-10-02 19:07:56 -0700

    a program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * @argc: the number of cl arguments
+ * @argv: array containing the cl arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
