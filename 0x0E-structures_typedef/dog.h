bb51f43ac4d1f6c3ae84123226aae038c18574af
Ogezi Emmanuel <Ogeziemmanuelsunday@gmail.com>
2023-10-10 16:47:55 -0700

    a type struct dog with name, age and owner as its types

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - a structure of dog
+ * @name: attribute pointer to the char name dog
+ * @age: attribute float age of dog
+ * @owner: attribute pointer to the char owner dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
